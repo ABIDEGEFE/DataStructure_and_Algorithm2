0f8591634ceb881218bc4835ee7aaea739177671
Abinet <agonaferdegefe@gmail.com>
2025-11-12 17:19:45 +0300

    a brief description of how Django channel enables real time communication with web Scoket protocol

--- Django/Channel_Practice/ChatApp/chat/consumers.py
+++ Django/Channel_Practice/ChatApp/chat/consumers.py
@@ -2,17 +2,38 @@
 import json
 
 from channels.generic.websocket import WebsocketConsumer
+from asgiref.sync import async_to_sync
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.roome_name = self.scope["url_route"]["kwargs"]["room_name"]
+        self.group_name = f"chat_{self.roome_name}"
+
+        async_to_sync(self.channel_layer.group_add)(
+            self.group_name, self.channel_name
+        )
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        async_to_sync(self.channel_layer.group_discard)(
+            self.group_name, self.channel_name
+        )
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.group_name,
+            {
+                "type": "chat.message",
+                "message": message,
+            },
+        )
+
+    def chat_message(self, event):
+        message = event["message"]
         self.send(text_data=json.dumps({"message": message}))
+
+    
